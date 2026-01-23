cbf0eb934af7c613e7f84bd53bab1c0c3acdf86b
Gar <gar@lonelydatum.com>
2026-01-23 13:57:54 -0500

    sym

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true,
+            drop_debugger: true,
+          },
+        },
+      }),
+    ],
+  },
+};
